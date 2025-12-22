8c05c7bdf5383f60b8b851ac88c0e866630f134b
xxr <3430709393@qq.com>
2025-12-22 23:13:53 +0800

    实现平台的左右来回移动

--- Assets/Script/PlatformMove.cs
+++ Assets/Script/PlatformMove.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class PlatformMove : MonoBehaviour
 {
     public Transform PosA,PosB;
+    private Transform MovePos;
     [SerializeField] private float MoveSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        MovePos=PosA;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position,PosA.position,MoveSpeed*Time.deltaTime);
+        if (Vector2.Distance(transform.position, PosA.position) < 0.1f)
+        {
+            MovePos=PosB;
+        }
+        if (Vector2.Distance(transform.position, PosB.position) < 0.1f)
+        {
+            MovePos=PosA;
+        }
+        transform.position = Vector2.MoveTowards(transform.position,MovePos.position,MoveSpeed*Time.deltaTime);
     }
 }
